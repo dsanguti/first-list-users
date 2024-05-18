23af4615a9fed98c0f40997b3a87f43eefd38e67
dsanguti <dsanguti@gmail.com>
2024-05-18 22:52:32 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="containerh1">
+        <h1>Holaa Mundo de nuevo</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
