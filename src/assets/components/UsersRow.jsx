b02ea20682b7cba6c4deab7048047622849ffd8b
dsanguti <dsanguti@gmail.com>
2024-05-29 08:41:15 +0200

    boton activar / desactivar

--- src/assets/components/UsersRow.jsx
+++ src/assets/components/UsersRow.jsx
@@ -1,18 +1,28 @@
 import style from "../css/UsersRow.module.css";
 import UserRole from "./UserRole";
 import UserStatus from "./UserStatus";
+import { useState } from "react";
 
 function UsersRow({ name, active, role = "profesor" }) {
+
+const [activeState, setActiveState] = useState(active);
+
+
   return (
     <div className={style.user}>
       <div className={style.name}>
         <span> {name}</span>
       </div>
       <div className={style.status}>
-        <UserStatus active={active} />
+        <UserStatus active={activeState} />
       </div>
       <div className={style.role}>
         <UserRole role={role} />
+      </div>
+      <div className={style.action}>
+        <button onClick={() => {setActiveState(!activeState)
+          
+        }}> {activeState ? "Desactivar" : "Activar"}</button>
       </div>
     </div>
   );
