dffb7c74d146786bfe8f22e22c2a8e4ce0a9557d
dsanguti <dsanguti@gmail.com>
2024-06-03 18:12:01 +0200

    Buscador al teclear

--- src/assets/components/UsersList.jsx
+++ src/assets/components/UsersList.jsx
@@ -1,17 +1,37 @@
 
 import style from '../css/UsersList.module.css';
 import UsersRow from "./UsersRow";
+import { useState } from 'react';
 
 
-function UsersList({ users, children }) {
-  const usersRendered = users.map((user) => (
-    <UsersRow key={user.name} {...user} />
-  ));
+function UsersList({ users }) {
+
+  const [search, setSearch] = useState('');
+
+  const usersRendered = filterAndRenderUsers(users, search);
+
   return (
     <div className={style.list}>
+      
+      <input type="text" name='search' placeholder = 'buscar usuarios...'value={search} onChange={ev => setSearch(ev.target.value)}></input>
       {usersRendered}
-      {children}
     </div>
   );
 }
+
+const filterAndRenderUsers = (users, search,)=>{
+  const normalizeSearch = search.toLowerCase();
+
+  const userFiltered = search 
+  ? users.filter(user => user.name.toLowerCase().startsWith(normalizeSearch)) 
+  : users;
+
+  const usersRendered = 
+  userFiltered.length > 0 ? (userFiltered.map(user => 
+    <UsersRow key={user.name} {...user} />)
+  ) : (<p>No hay Usuarios</p>);
+
+  return usersRendered;
+}
+
 export default UsersList;
