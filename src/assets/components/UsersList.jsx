542515ba595a1f522df6b5dfb0706d7bc4f7baee
dsanguti <dsanguti@gmail.com>
2024-07-22 10:59:30 +0200

    userList Refatory

--- src/assets/components/UsersList.jsx
+++ src/assets/components/UsersList.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import style from "../css/UsersList.module.css";
 import UsersListFilters from "./UsersListFilters";
 import UsersListRows from "./UsersListRows";
+import { filterActiveUsers, filterUsersByName, sortUsers } from "./lib/users/FilterUsers";
+import { useFilters } from "./lib/hooks/useFilters";
+
 
 function UsersList({ initialUsers }) {
   const { search, onlyActive, sortBy, setSearch, setOnlyActive, setSortBy } =
@@ -29,82 +32,7 @@ function UsersList({ initialUsers }) {
   );
 }
 
-const filterUsersByName = (users, search) => {
-  if (!search) return [...users];
 
-  const lowerCaseSearch = search.toLowerCase();
-
-  return users.filter((user) =>
-    user.name.toLowerCase().includes(lowerCaseSearch)
-  );
-};
-
-const filterActiveUsers = (users, active) => {
-  if (!active) return [...users];
-
-  return users.filter((user) => user.active);
-};
-
-const sortUsers = (users, sortBy) => {
-  const sortedUsers = [...users];
-
-  switch (sortBy) {
-    case 1:
-      return sortedUsers.sort((a, b) => {
-        if (a.name > b.name) return 1;
-        if (a.name < b.name) return -1;
-        return 0;
-      });
-    case 2:
-      return sortedUsers.sort((a, b) => {
-        if (a.role === b.role) return 0;
-        if (a.role === "teacher") return -1;
-        if (a.role === "student" && b.role === "other") return -1;
-        return 1;
-      });
-    case 3:
-      return sortedUsers.sort((a, b) => {
-        if (a.active === b.active) return 0;
-        if (a.active && !b.active) return -1;
-        return 1;
-      });
-    default:
-      return sortedUsers;
-  }
-};
-
-const useFilters = () => {
-  const [filters, setFilters] = useState({
-    search: "",
-    onlyActive: false,
-    sortBy: 0,
-  });
-  const setSearch = (search) =>
-    setFilters({
-      ...filters,
-      search,
-    });
-
-  const setSortBy = (sortBy) =>
-    setFilters({
-      ...filters,
-      sortBy,
-    });
-
-  const setOnlyActive = (onlyActive) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      onlyActive,
-      sortBy: onlyActive && prevFilters.sortBy === 3 ? 0 : prevFilters.sortBy,
-    }));
-  };
-  return {
-    ...filters,
-    setSearch,
-    setSortBy,
-    setOnlyActive,
-  };
-};
 
 const useUsers = (initialUsers) => {
   const [users, setUsers] = useState(initialUsers);
